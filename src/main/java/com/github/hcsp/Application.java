3f0bcf811a277c85b5a94deede20318528a05c1d
baixc <969552017@qq.com>
2020-01-25 15:33:40 +0800

    修改资源路径 将非java的Mapper.xml 文件放到resources中去

--- src/main/java/com/github/hcsp/Application.java
+++ src/main/java/com/github/hcsp/Application.java
@@ -1,11 +1,9 @@
 package com.github.hcsp;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(value = "com.github.hscp.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
