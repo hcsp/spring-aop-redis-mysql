01f9ac4f6a4723dc092a6bd507519abd4c3a66dc
zaker-666 <1075151259china@gmail.com>
2020-02-02 20:28:31 +0800

    push the result

--- src/main/java/com/github/hcsp/CacheAspect.java
+++ src/main/java/com/github/hcsp/CacheAspect.java
@@ -18,28 +18,30 @@ public class CacheAspect {
     static class CacheValue implements Serializable {
         @NotNull Object rankItemCache;
         @NotNull long time;
+
         public CacheValue(Object rankItemCache, long time) {
             this.rankItemCache = rankItemCache;
             this.time = time;
         }
     }
+
     @Autowired
     RedisTemplate<String, Object> redisTemplate;
 
     @Around("@annotation(com.github.hcsp.anno.Cache)")
     public Object cache(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        MethodSignature signature = (MethodSignature)proceedingJoinPoint.getSignature();
+        MethodSignature signature = (MethodSignature) proceedingJoinPoint.getSignature();
         String name = signature.getName();
         int myTime = signature.getMethod().getAnnotation(Cache.class).cacheSeconds();
         CacheValue cacheValue = (CacheValue) redisTemplate.opsForValue().get(name);
-        if(cacheValue!=null&&(System.currentTimeMillis()-cacheValue.time)<myTime*1000){
+        if (cacheValue != null && (System.currentTimeMillis() - cacheValue.time) < myTime * 1000) {
             System.out.println("get from Cache");
             return cacheValue.rankItemCache;
-        }else {
+        } else {
             System.out.println("get from Database");
             Object realValue = proceedingJoinPoint.proceed();
-            CacheValue myCacheValue = new CacheValue(realValue,System.currentTimeMillis());
-            redisTemplate.opsForValue().set(name,myCacheValue);
+            CacheValue myCacheValue = new CacheValue(realValue, System.currentTimeMillis());
+            redisTemplate.opsForValue().set(name, myCacheValue);
             return realValue;
         }
     }
