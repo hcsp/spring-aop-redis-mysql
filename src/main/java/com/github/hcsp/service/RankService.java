b5f062325fe81137d1d95390064553d293a3d889
xdc <1434412784@qq.com>
2020-02-09 10:32:11 +0800

    删除引入多余的包

--- src/main/java/com/github/hcsp/service/RankService.java
+++ src/main/java/com/github/hcsp/service/RankService.java
@@ -3,7 +3,6 @@ package com.github.hcsp.service;
 import com.github.hcsp.anno.Cache;
 import com.github.hcsp.dao.RankDao;
 import com.github.hcsp.entity.RankItem;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
