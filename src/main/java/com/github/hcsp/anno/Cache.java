8f2b9ae65f50621ab8dc8c32afefbe92f666efa9
zhy <809775352qq.com>
2020-02-14 11:55:53 +0800

    Spring+AOP+Redis+MySQL练习

--- src/main/java/com/github/hcsp/anno/Cache.java
+++ src/main/java/com/github/hcsp/anno/Cache.java
@@ -5,5 +5,6 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Cache {
-    int cacheSeconds() default 1;
+    int cacheSeconds()
+            default 1;
 }
