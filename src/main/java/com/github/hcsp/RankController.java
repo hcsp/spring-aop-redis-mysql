2d70a09bfc52af5af76eb00b32b249c54c83a1c0
zhy533726735220 <809775352@qq.com>
2020-02-19 12:21:05 +0800

    Spring+AOP+Redis+MySQL练习 (#13)
    Spring+AOP+Redis+MySQL练习 (#13)

---
+++ src/main/java/com/github/hcsp/RankController.java
@@ -0,0 +1,28 @@
+package com.github.hcsp;
+
+import com.github.hcsp.entity.RankItem;
+import com.github.hcsp.service.RankService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+public class RankController {
+
+    @Autowired
+    private RankService rankService;
+
+    @RequestMapping(value = "/rank.htm")
+    @ResponseBody
+    public ModelAndView search() {
+        List<RankItem> rankItemList = rankService.selectRank();
+        HashMap<String, Object> model = new HashMap<>();
+        model.put("items", rankItemList);
+        return new ModelAndView("rank", model);
+    }
+}
