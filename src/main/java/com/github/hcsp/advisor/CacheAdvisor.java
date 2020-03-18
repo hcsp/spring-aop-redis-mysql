f6ed504d9a60c7989d8b012a2ba6dfe324ce5d10
Lee <lizhijie_lee@126.com>
2020-03-19 00:39:43 +0800

    使用redis缓存过期时间

--- src/main/java/com/github/hcsp/advisor/CacheAdvisor.java
+++ src/main/java/com/github/hcsp/advisor/CacheAdvisor.java
@@ -7,12 +7,12 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Description:
@@ -25,32 +25,23 @@ import java.util.List;
 public class CacheAdvisor {
     @Autowired
     private RedisTemplate redisTemplate;
-    private long lastMilliTime = 0;
 
     @Around("@annotation(com.github.hcsp.myAnnotation.Cache)")
     private List<GoodsDealInfo> checkCacheBeforeFetchFromDataBase(ProceedingJoinPoint pjp) throws Throwable {
         String methodName = pjp.getSignature().getName();
-        List<GoodsDealInfo> model;
-        if (isValidCacheTime(pjp)) {
-            System.out.println("走缓存");
-            HashOperations<String, String, List<GoodsDealInfo>> res = redisTemplate.opsForHash();
-            model = res.get(methodName, "map");
-        } else {
-            System.out.println("走数据库");
-            model = (List<GoodsDealInfo>) pjp.proceed();
-            redisTemplate.opsForHash().put(methodName, "map", model);
-        }
-        return model;
-    }
-
-    private boolean isValidCacheTime(ProceedingJoinPoint pjp) {
+        Object model;
         MethodSignature signature = (MethodSignature) pjp.getSignature();
         Method method = signature.getMethod();
         long validCacheTime = method.getAnnotation(Cache.class).value();
-        long currentTimeMillis = System.currentTimeMillis();
-        long duration = currentTimeMillis - lastMilliTime;
-        boolean flag = duration <= (validCacheTime * 1000);
-        lastMilliTime = currentTimeMillis;
-        return flag;
+        if (redisTemplate.hasKey(methodName)) {
+            System.out.println("走缓存");
+            model = redisTemplate.opsForValue().get(methodName);
+        } else {
+            System.out.println("走数据库");
+            model = pjp.proceed();
+            redisTemplate.opsForValue().set(methodName, model);
+            redisTemplate.expire(methodName, validCacheTime, TimeUnit.SECONDS);
+        }
+        return (List<GoodsDealInfo>) model;
     }
 }
