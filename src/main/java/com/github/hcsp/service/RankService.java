667ead47ba9d5259335e55f329a3c947c15607ba
wangyujie1207 <82414199@163.com>
2020-03-27 01:20:55 +0800

    听课是一回事儿 做是另一回事儿 (#22)
    听课是一回事儿 做是另一回事儿 (#22)

---
+++ src/main/java/com/github/hcsp/service/RankService.java
@@ -0,0 +1,22 @@
+package com.github.hcsp.service;
+
+import com.github.hcsp.annotation.Cache;
+import com.github.hcsp.dao.RankDao;
+import com.github.hcsp.entity.RankItem;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RankService {
+    private final RankDao rankDao;
+
+    public RankService(RankDao rankDao) {
+        this.rankDao = rankDao;
+    }
+
+    @Cache
+    public List<RankItem> getRank() {
+        return rankDao.getRank();
+    }
+}
