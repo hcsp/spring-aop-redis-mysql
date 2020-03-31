a615c3bc72231b22b1e8f36d24cc2bb36a723d10
ws1603 <48832455+ws1603@users.noreply.github.com>
2020-03-31 14:40:37 +0800

    Spring+AOP+Redis+MySQL练习 (#25)
    Spring+AOP+Redis+MySQL练习 (#25)

---
+++ src/main/java/com/github/hcsp/dao/GoodsMapper.java
@@ -0,0 +1,14 @@
+package com.github.hcsp.dao;
+
+import com.github.hcsp.entity.Goods;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface GoodsMapper {
+    @Select("select * from goods where id = #{id}")
+    Goods getGoodsById(@Param("id") Integer id);
+}
+
+
