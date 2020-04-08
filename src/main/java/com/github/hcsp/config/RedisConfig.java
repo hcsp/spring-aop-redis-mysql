48c4910d5d58fff467fd54ef286b4a2fcbff2ab2
Felix <fiymi666@gmail.com>
2020-04-08 21:27:40 +0800

    删掉无用的导入

--- src/main/java/com/github/hcsp/config/RedisConfig.java
+++ src/main/java/com/github/hcsp/config/RedisConfig.java
@@ -2,7 +2,6 @@ package com.github.hcsp.config;
 
 import com.github.hcsp.entity.RankItem;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
