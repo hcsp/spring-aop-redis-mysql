a3674821c478a939536bd565e0157a5f6454ebc8
Harimoto Satoshi <zhanglevan@gmail.com>
2020-04-21 21:08:55 +0800

    Satoshi (#34)
    Satoshi (#34)

---
+++ src/main/java/com/github/hcsp/config/RedisConfig.java
@@ -0,0 +1,17 @@
+package com.github.hcsp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        return template;
+    }
+}
