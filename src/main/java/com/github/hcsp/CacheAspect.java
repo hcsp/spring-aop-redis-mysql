c4f9c8d49e367ed7ed188778a5ba05f619836073
shiliyu <liyu.italy@hotmail.com>
2020-04-26 04:36:42 +0800

    DAMN soap!!

--- src/main/java/com/github/hcsp/CacheAspect.java
+++ src/main/java/com/github/hcsp/CacheAspect.java
@@ -1,51 +1,55 @@
 package com.github.hcsp;
 
+import com.github.hcsp.annotation.Cache;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
 
-import javax.annotation.Resource;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 @Aspect
 @Configuration
 public class CacheAspect {
-    @Resource
+    @Autowired
     RedisTemplate<Object, Object> redisTemplate;
 
     @Around("@annotation(com.github.hcsp.annotation.Cache)")
     public Object cache(final ProceedingJoinPoint joinPoint) throws Throwable {
-        MethodSignature signature = (MethodSignature)joinPoint.getSignature();
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        long timeout = signature.getMethod().getAnnotation(Cache.class).cacheSeconds();
         String methodName = signature.getName();
+
         String className = joinPoint.getTarget().getClass().getName();
         Object[] args = joinPoint.getArgs();
 
-        Object cacheKey = (Object) new CacheKey(methodName, className, args);
+        Object cacheKey = new CacheKey(methodName, className, args);
 
         Object cachedValue = redisTemplate.opsForValue().get(cacheKey);
 
         if (cachedValue != null) {
-            System.out.println("From cache!");
+            System.out.println("From cache");
             return cachedValue;
         } else {
             System.out.println("From database!");
             Object actualValue = joinPoint.proceed();
-            redisTemplate.opsForValue().set(cacheKey, actualValue);
+            redisTemplate.opsForValue().set(cacheKey, actualValue, timeout, TimeUnit.SECONDS);
             return actualValue;
         }
     }
 
-    public static class CacheKey implements Serializable {
+    static class CacheKey implements Serializable {
         private String methodName;
         private String className;
         private Object[] arguments;
 
-        public CacheKey(String methodName, String className, Object[] arguments) {
+        CacheKey(String methodName, String className, Object[] arguments) {
             this.methodName = methodName;
             this.className = className;
             this.arguments = arguments;
@@ -53,8 +57,12 @@ public class CacheAspect {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
             CacheKey cacheKey = (CacheKey) o;
             return methodName.equals(cacheKey.methodName) &&
                     className.equals(cacheKey.className) &&
--- src/main/java/com/github/hcsp/dao/UserMapper.java
+++ src/main/java/com/github/hcsp/dao/UserMapper.java
@@ -8,5 +8,5 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface UserMapper {
     @Select("select * from user where id = #{id}")
-    public User getUserById(@Param("id")Integer id);
+    User getUserById(@Param("id")Integer id);
 }
