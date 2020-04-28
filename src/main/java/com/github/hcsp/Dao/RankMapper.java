eda3cd6130c1ffbab216009b72b05cbd2eb592eb
frank <52615871+feiyongjing@users.noreply.github.com>
2020-04-28 23:01:30 +0800

    Spring+AOP+Redis+MySQL练习 (#36)
    Spring+AOP+Redis+MySQL练习 (#36)

---
+++ src/main/java/com/github/hcsp/Dao/RankMapper.java
@@ -0,0 +1,19 @@
+package com.github.hcsp.Dao;
+
+import com.github.hcsp.entity.RankingList;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Mapper
+public interface RankMapper {
+    @Select("select goods.name as goodsName, sum(`order`.price*`order`.quantity) as Turnover from goods\n" +
+            "left join `order`\n" +
+            "on `order`.goods_id=goods.id\n" +
+            "group by goods.id\n" +
+            "order by Turnover desc")
+    List<RankingList> sortTheAmountOfGoodsSold();
+}
---
+++ src/main/java/com/github/hcsp/entity/RankingList.java
@@ -0,0 +1,24 @@
+package com.github.hcsp.entity;
+
+import java.io.Serializable;
+
+public class RankingList implements Serializable {
+    private String goodsName;
+    private int Turnover;
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public int getTurnover() {
+        return Turnover;
+    }
+
+    public void setTurnover(int turnover) {
+        this.Turnover = turnover;
+    }
+}
