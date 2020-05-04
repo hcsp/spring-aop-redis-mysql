5faed3e48ec9bd1defdee6d12346379815a9278b
HAD <had13625428701@aliyun.com>
2020-05-04 16:58:45 +0800

    springbootdemo sql改动

--- src/main/java/com/github/hcsp/mapper/RankMapper.java
+++ src/main/java/com/github/hcsp/mapper/RankMapper.java
@@ -9,10 +9,10 @@ import java.util.List;
 @Mapper
 public interface RankMapper {
     @Select("select g.`name`,t.price\n" +
-            "FROM (\n" +
-            "\tselect goods_id goodsId,sum(price * quantity) as price\n" +
-            "from `order`\n" +
-            "GROUP BY goods_id\n" +
-            ") t JOIN goods g ON t.goodsId = g.id ORDER BY t.price DESC")
+            "            FROM (\n" +
+            "            select goods_id goodsId,ifnull(sum(price * quantity),0) as price\n" +
+            "            from `order`\n" +
+            "            GROUP BY goods_id\n" +
+            "            ) t RIGHT JOIN goods g ON t.goodsId = g.id ORDER BY t.price DESC")
     List<Rank> getRankList();
 }
