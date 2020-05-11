e8031347c7bd933651aaa10ec635e9144ab1bb89
leo2c <z429882801@qq.com>
2020-05-12 01:29:51 +0800

    删除无用的导入

--- src/main/java/com/github/hcsp/mall/controller/RankController.java
+++ src/main/java/com/github/hcsp/mall/controller/RankController.java
@@ -2,7 +2,6 @@ package com.github.hcsp.mall.controller;
 
 import com.github.hcsp.mall.service.RankService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
