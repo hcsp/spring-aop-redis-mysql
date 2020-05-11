6a3978df8acf6c403ebf1a2552b62fd3b29cd6bc
leo2c <53816081+leo2c@users.noreply.github.com>
2020-05-12 01:42:41 +0800

    Spring+AOP+Redis+MySQL练习 (#38)
    Spring+AOP+Redis+MySQL练习 (#38)

---
+++ src/main/java/com/github/hcsp/mall/service/RankService.java
@@ -0,0 +1,9 @@
+package com.github.hcsp.mall.service;
+
+import com.github.hcsp.mall.entity.RankItem;
+
+import java.util.List;
+
+public interface RankService {
+    List<RankItem> getRankItems();
+}
