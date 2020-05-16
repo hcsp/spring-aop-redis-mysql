c13f9b504f9882766e1b82198350100c5070702a
arsonalXxu <897386402@qq.com>
2020-05-17 03:52:09 +0800

    spring aop redis mysql practise (#39)
    spring aop redis mysql practise (#39)

--- src/main/java/com/github/hcsp/Application.java
+++ src/main/java/com/github/hcsp/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@MapperScan(value = "hcsp")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
