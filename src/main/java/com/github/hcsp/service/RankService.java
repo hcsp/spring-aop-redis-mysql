32ecd86a42d97fefc5b9edf237904cf5e804e412
JonesWilde <davidgluttongk@gmail.com>
2020-05-22 13:33:55 -0700

    第三次尝试

---
+++ src/main/java/com/github/hcsp/service/RankService.java
@@ -0,0 +1,18 @@
+package com.github.hcsp.service;
+
+import com.github.hcsp.dao.RankDao;
+import com.github.hcsp.entity.RankItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RankService {
+    @Autowired
+    private RankDao rankDao;
+
+    public List<RankItem> getRank() {
+        return rankDao.getRank();
+    }
+}
