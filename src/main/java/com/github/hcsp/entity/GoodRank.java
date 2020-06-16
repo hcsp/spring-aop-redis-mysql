65a01f262fa5516e9289463659adc4532e7f3206
kongmu373 <kongmu1235@gmail.com>
2020-06-16 10:57:41 +0800

    完成Spring+AOP+Redis+MySQL练习

---
+++ src/main/java/com/github/hcsp/entity/GoodRank.java
@@ -0,0 +1,33 @@
+package com.github.hcsp.entity;
+
+import java.io.Serializable;
+
+public class GoodRank implements Serializable {
+
+    private String name;
+    private Integer amount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "GoodRank{" +
+                "name='" + name + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+}
