ff94a0f097dc70ed45487ac296e7b3e5f7bddb37
lazyben <458861463@qq.com>
2020-07-12 16:16:33 +0800

    Spring+AOP+Redis+MySQL练习

---
+++ src/main/java/com/github/hcsp/CacheAspect.java
@@ -0,0 +1,30 @@
+package com.github.hcsp;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+@Aspect
+@Configuration
+public class CacheAspect {
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
+
+    @Around("@annotation(com.github.hcsp.anno.Cache)")
+    public Object cache(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        String name = signature.getName();
+        Object cacheValue = redisTemplate.opsForValue().get(name);
+        if (cacheValue == null) {
+            cacheValue = joinPoint.proceed();
+            redisTemplate.opsForValue().set(name, cacheValue, 1, TimeUnit.SECONDS);
+        }
+        return cacheValue;
+    }
+}
