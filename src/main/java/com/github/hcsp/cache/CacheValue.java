5438913513f6b69d558580345813f8d5d95c0f49
豪 <linesahao@outlook.com>
2020-07-14 15:10:30 +0800

    Spring boot + Aop + Mysql + Redis. (#50)
    Spring boot + Aop + Mysql + Redis. (#50)

---
+++ src/main/java/com/github/hcsp/cache/CacheValue.java
@@ -0,0 +1,29 @@
+package com.github.hcsp.cache;
+
+import java.io.Serializable;
+
+public class CacheValue implements Serializable {
+    private Object result;
+    private long processTime;
+
+    public CacheValue(Object result, long processTime) {
+        this.result = result;
+        this.processTime = processTime;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
+
+    public long getProcessTime() {
+        return processTime;
+    }
+
+    public void setProcessTime(long processTime) {
+        this.processTime = processTime;
+    }
+}
