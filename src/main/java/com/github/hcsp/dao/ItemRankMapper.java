d29070b604370a512eef1274dabfefcf93f231f5
Sunpeng526 <215860865@qq.com>
2020-08-15 15:27:56 +0800

    submit import fixes

--- src/main/java/com/github/hcsp/dao/ItemRankMapper.java
+++ src/main/java/com/github/hcsp/dao/ItemRankMapper.java
@@ -3,11 +3,10 @@
  */
 package com.github.hcsp.dao;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author sunp
