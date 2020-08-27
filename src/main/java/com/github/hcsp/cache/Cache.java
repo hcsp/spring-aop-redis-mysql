2aed077e4977ab9b5aca53d2389f10d5a6455a99
郑海川 <53638659+lance-zheng66@users.noreply.github.com>
2020-08-27 12:40:56 +0800

    Spring AOP/MySQL/Redis实战 (#56)
    Spring AOP/MySQL/Redis实战 (#56)

---
+++ src/main/java/com/github/hcsp/cache/Cache.java
@@ -0,0 +1,12 @@
+package com.github.hcsp.cache;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Cache {
+
+}
+
+
+
