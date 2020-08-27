ebf819bed639c646bf8de4b8e529bed81c05f596
lance <491799643@qq.com>
2020-08-27 12:13:17 +0800

    Spring AOP/MySQL/Redis实战

---
+++ src/main/java/com/github/hcsp/dao/RankDao.java
@@ -0,0 +1,19 @@
+package com.github.hcsp.dao;
+
+import com.github.hcsp.entity.Rank;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RankDao {
+
+  @Autowired
+  SqlSession sqlSession;
+
+  public List<Rank> getRank() {
+    return sqlSession.selectList("RankMapper.getRank");
+  }
+}
