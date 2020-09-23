f34f38afb5fd31fff35dc11ed79ca145672e54ea
nicknessii <545310317@qq.com>
2020-09-23 12:43:13 +0800

    Spring AOP/MySQL/Redis实战

---
+++ src/main/java/com/github/hcsp/config/MyConfiguration.java
@@ -0,0 +1,19 @@
+package com.github.hcsp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+// 这里可以声明一个复杂的bean
+@Configuration
+public class MyConfiguration {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+        return redisTemplate;
+    }
+}
+
