7e2197daeaed2b710880d7865781b480c1e29564
pccmast <35651458+pccmast@users.noreply.github.com>
2021-03-28 23:19:03 +0800

    使用mybatis完成任务 (#81)
    使用mybatis完成任务 (#81)

---
+++ src/main/java/com/github/hcsp/services/RankOrderService.java
@@ -0,0 +1,18 @@
+package com.github.hcsp.services;
+
+import com.github.hcsp.dao.RankOrderDao;
+import com.github.hcsp.entities.RankItem;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class RankOrderService {
+    @Resource
+    RankOrderDao rankOrderDao;
+
+    public List<RankItem> doService() {
+        return rankOrderDao.doRankOrder();
+    }
+}
