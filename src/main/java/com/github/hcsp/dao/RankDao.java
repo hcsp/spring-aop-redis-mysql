56887c98e829fd39485b02502f38fcc6342950d4
MongieLee <le30n@outlook.com>
2021-11-20 20:55:57 +0800

    Spring+AOP+Redis+MySQL练习

--- src/main/java/com/github/hcsp/dao/RankDao.java
+++ src/main/java/com/github/hcsp/dao/RankDao.java
@@ -2,7 +2,6 @@ package com.github.hcsp.dao;
 
 import com.github.hcsp.entity.RankItem;
 import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
